f6e8c3b4f9371a7bd8d61d3667200f3b075ee36b
Robert Magier <robert.magier@devapo.pl>
2025-02-21 17:53:08 +0100

    Add API function to get WithdrawEvent

--- apps/api/src/eventlistener/eventlistener.service.ts
+++ apps/api/src/eventlistener/eventlistener.service.ts
@@ -2,11 +2,12 @@ import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { ethers } from 'ethers';
 import { FUSDC_ABI__factory } from 'src/contracts';
 import { FUSDC_ABI, FUSDC_ABIInterface } from 'src/contracts/FUSDC_ABI';
-import { DepositEventDetails } from './eventslistener.types';
+import { DepositEventDetails, WithdrawEventDetails } from './eventslistener.types';
 import BigNumber from 'bignumber.js';
 
 const CHUNK_SIZE = 100;
-const HISTORY_BLOCKS = 2000;
+const HISTORY_BLOCKS = 3000;
+const PROVIDER_URL = 'https://base-mainnet.infura.io/v3';
 
 @Injectable()
 export class EventListenerService implements OnModuleInit {
@@ -14,18 +15,18 @@ export class EventListenerService implements OnModuleInit {
   private provider: ethers.JsonRpcProvider;
   private contract: FUSDC_ABI;
   private interface = new ethers.Interface(FUSDC_ABI__factory.abi) as FUSDC_ABIInterface;
-
+  private FUSDC_ADDRESS = process.env.FUSDC_ADDRESS;
   constructor() {
     this.logger.log('EventListenerService initialized');
   }
 
   async onModuleInit() {
     this.logger.log('EventListenerService is running');
-    this.provider = new ethers.JsonRpcProvider('https://base-mainnet.infura.io/v3/3373edb6a0524e44a61b76d03541e5c1');
+    this.provider = new ethers.JsonRpcProvider(`${PROVIDER_URL}/${process.env.INFURA_API_KEY}`);
     this.provider.getNetwork().then((network) => {
       this.logger.log(`Network: ${network.name}`);
     });
-    this.contract = FUSDC_ABI__factory.connect('0xf42f5795D9ac7e9D757dB633D693cD548Cfd9169', this.provider);
+    this.contract = FUSDC_ABI__factory.connect(this.FUSDC_ADDRESS, this.provider);
     const latestBlock: ethers.BlockTag = await this.provider.getBlockNumber();
     await this.getHistoricalLogs(latestBlock - HISTORY_BLOCKS, latestBlock);
 
@@ -34,28 +35,38 @@ export class EventListenerService implements OnModuleInit {
     });
   }
 
-  private async getDepositEvent(fromBlock: BigInt, toBlock: BigInt): Promise<DepositEventDetails[]> {
-    const depositFilter = this.contract.filters.Deposit(null, null, null);
-    const address = await this.contract.getAddress();
-    const topics = await depositFilter.getTopicFilter();
-
-    const data: DepositEventDetails[] = [] as DepositEventDetails[];
+  private async getEvent<T extends DepositEventDetails | WithdrawEventDetails>(
+    fromBlock: BigInt,
+    toBlock: BigInt,
+    topics: ethers.TopicFilter,
+  ): Promise<T[]> {
+    const data: T[] = [] as T[];
     const logs = await this.provider.getLogs({
       fromBlock: fromBlock as ethers.BlockTag,
       toBlock: toBlock as ethers.BlockTag,
-      address,
+      address: this.FUSDC_ADDRESS,
       topics,
     });
     logs.forEach((log) => {
-      const parsedLog = this.interface.parseLog(log);
-      this.logger.log(`Block number: ${log.blockNumber} ${parsedLog.name} Index: ${log.index}`);
-      const logData = this.parseLog<DepositEventDetails>(log);
+      const logData = this.parseLog<T>(log);
       data.push(logData);
     });
     return data;
   }
 
-  private parseLog<T extends DepositEventDetails>(log: ethers.Log): T {
+  private async getDepositEvent(fromBlock: BigInt, toBlock: BigInt): Promise<DepositEventDetails[]> {
+    const depositFilter = this.contract.filters.Deposit(null, null, null);
+    const topics = await depositFilter.getTopicFilter();
+    return this.getEvent<DepositEventDetails>(fromBlock, toBlock, topics);
+  }
+
+  private async getWithdrawEvent(fromBlock: BigInt, toBlock: BigInt): Promise<WithdrawEventDetails[]> {
+    const withdrawFilter = this.contract.filters.Withdraw(null, null, null);
+    const topics = await withdrawFilter.getTopicFilter();
+    return this.getEvent<WithdrawEventDetails>(fromBlock, toBlock, topics);
+  }
+
+  private parseLog<T extends DepositEventDetails | WithdrawEventDetails>(log: ethers.Log): T {
     const parsedLog = this.interface.parseLog(log);
     const logData: T = {} as T;
     parsedLog.fragment.inputs.forEach((input, index) => {
@@ -65,7 +76,7 @@ export class EventListenerService implements OnModuleInit {
     logData.block = log.blockNumber;
     logData.index = log.index;
     return logData;
-    }
+  }
 
   private async getHistoricalLogs(startBlock: ethers.BlockTag, endBlock: ethers.BlockTag): Promise<void> {
     const fromBlock: BigNumber = BigNumber(startBlock.toString());
@@ -79,9 +90,13 @@ export class EventListenerService implements OnModuleInit {
       }
 
       this.logger.debug('Getting logs for block range: ' + start.toString() + ' to ' + end.toString());
-      const deposits = await this.getDepositEvent(BigInt(start.toString()), BigInt(end.toString()));
 
+      const deposits = await this.getDepositEvent(BigInt(start.toString()), BigInt(end.toString()));
+      const withdraws = await this.getWithdrawEvent(BigInt(start.toString()), BigInt(end.toString()));
+      
       console.log(deposits);
+      console.log(withdraws);
+
       start = start.plus(CHUNK_SIZE);
     }
   }
