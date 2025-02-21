f2aa317ea8f419b71edebda7b9ed913a74a9c230
Robert Magier <robert.magier@devapo.pl>
2025-02-21 10:16:18 +0100

    Use API_PORT from .env file to get listening PORT on api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.API_PORT ?? 3000;
+  logger.log(`Application listening on port ${PORT}`);
   await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
